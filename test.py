192aaa27693abca21e7181fb30efcc83bdb63592
Alekcey264 <alekseverevkin@gmail.com>
2024-12-15 18:09:08 +0500

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+def my_sf():
+    print("What an awesome print!")
+
+def main():
+    print("Git Program")
+    print("and i change it")
+    print("and again")
+    print("and one more time")
+    my_sf()
+
+if __name__ == "__main__":
+    main()
